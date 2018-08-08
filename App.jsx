b514171ab6b67ffa2250a1bcc1a1d524da98cab0
chandran M <chandru.m@outlook.com>
2018-08-08 20:02:55 +0530

    Reactjs

--- App.jsx
+++ App.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   render() {
+    return <div>Test</div>;
+  }
+}
+
+class SecondExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicks: 0 };
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      clicks: prevState.clicks + 1
+    }));
+  }
+
+  render() {
     return (
-      <div>Test</div>
+      <button onClick={this.handleClick.bind(this)}>
+        Clicked {this.state.clicks} Times
+      </button>
     );
   }
 }
 
-export default App;
+export { App, SecondExample };
--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App.jsx';
+import {
+    App,
+    SecondExample
+} from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render( < App / > , document.getElementById('app'));
+
+ReactDOM.render( < SecondExample / > , document.getElementById('app1'));
